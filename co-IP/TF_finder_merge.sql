b36722edb421c19163f8322f0a1c39be472fa96a
mvsanghvi <mevsanghvi@gmail.com>
2024-12-16 15:45:47 -0600

    Script edits

--- co-IP/TF_finder_merge.sql
+++ co-IP/TF_finder_merge.sql
@@ -1,6 +1,6 @@
 SELECT WTYT.*
-FROM main."11122024_WTYAP_WTTAZ_PCF_MS" as WTYT
+FROM main."12052024_WTTAZ_Y2TAZ_MS" AS WTYT
 Left join main."TFlink_Homo_sapiens_browse" as TF on WTYT."Gene Symbol"=TF."Gene Symbol"
-WHERE TF.Type LIKE 'transcription factor%'
-ORDER BY TF."Gene Symbol" ASC 
+WHERE TF.Type LIKE 'transcription factor%' AND WTYT."t=24; WT TAZ; Abundance 1226504" IS NOT NULL AND WTYT."t=24; Y2 TAZ; Abundance 1226508" IS NOT NULL
+ORDER BY WTYT."t=24; WT TAZ; Abundance 1226504" DESC 
 	
