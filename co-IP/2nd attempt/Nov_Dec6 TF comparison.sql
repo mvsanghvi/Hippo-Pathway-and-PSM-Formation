46ba81a5daa6a657c4c2e1afa3812eecbd446e2c
mvsanghvi <mevsanghvi@gmail.com>
2024-12-20 11:53:46 -0600

    New code

--- co-IP/2nd attempt/Nov_Dec6 TF comparison.sql
+++ co-IP/2nd attempt/Nov_Dec6 TF comparison.sql
@@ -1,3 +1,4 @@
-SELECT ntwelve.*
+SELECT ntwelve."Gene Symbol",ntwelve." t=24 (NMP); WT YAP; Abundance 1221004 ",dthree." t=24; WTYAP; Abundance.QEX3_1226029 " 
 FROM main."11122024_TFtable" as ntwelve
 Join main."12032024_TFtable" as dthree on ntwelve."Gene Symbol"=dthree."Genes"
+WHERE ntwelve." t=24 (NMP); WT YAP; Abundance 1221004 " IS NOT NULL AND dthree." t=24; WTYAP; Abundance.QEX3_1226029 " IS NOT NULL
