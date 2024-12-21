f1f57bd38c831a0b5d8e1dc84fa258cdcd363f8d
mvsanghvi <mevsanghvi@gmail.com>
2024-12-21 14:08:32 -0600

    New Code

---
+++ co-IP/2nd attempt/YAP_v_TAZ_WT.sql
@@ -0,0 +1,5 @@
+SELECT WTYAP.*,WTTAZ."t=24 (NMP); WT TAZ; Abundance 1221008",WTTAZ."t=24; WT TAZ; Abundance 1226504"
+FROM main.WTYAP_Dec3_Nov as WTYAP
+Join main.WTTAZ_Dec5_Nov as WTTAZ on WTYAP."Gene Symbol"=WTTAZ."Gene Symbol" 
+
+	
